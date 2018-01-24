e8c6709f925288320c1d01cb25c039c76f9d183f
Raven Demeyer <raven.demeyer@gmail.com>
2018-01-24 16:57:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ng-packaged TickTock Demo
+
+* Tick Tock library by [trekhleb](https://github.com/trekhleb/angular-library-seed)
+* Deployment of a simple component and service library using [ng-packagr](https://github.com/dherges/ng-packagr). Some core features include:
+  * Inlined templates and stylesheets
+  * CSS preprocessing and prefixes
+  * Compiled for FESM2015, FESM5, UMD with type definitions and AoT metadata 
+* Built following the [Angular Package Format](https://docs.google.com/document/d/1CZC2rcpxffTDfRDs6p1cfbmKNLA6x5O-NtkJglDaBVs/edit#) guidelines
+
+### Possible improvements
+* Various tools for testing
+* Semantic Versioning
+* Deployment tools
---
+++ src/services/tick-tock/tick-tock.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+/**
+ * TickTockService class.
+ */
+@Injectable()
+export class TickTockService {
+
+  /**
+   * Extend time value with zero if required.
+   */
+  private static formatTimeNumber(value: number): string {
+    const stringValue = value.toString();
+    return ('0' + stringValue).slice(-2);
+  }
+
+  /**
+   * Get current time string.
+   */
+  private static getNowString(): string {
+    const date = new Date();
+
+    const hours = TickTockService.formatTimeNumber(date.getHours());
+    const minutes = TickTockService.formatTimeNumber(date.getMinutes());
+    const seconds = TickTockService.formatTimeNumber(date.getSeconds());
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  /**
+   * Set up timer frequency.
+   */
+  private readonly TIMEOUT: number = 1000;
+
+  /**
+   * Get current time observable.
+   */
+  public getTick(): Observable<string> {
+    return Observable
+      .timer(0, this.TIMEOUT)
+      .map((tick) => TickTockService.getNowString());
+  }
+}
